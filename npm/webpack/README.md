b3e85074e776c1c81dc59a3fa92ec31348c4ea57
juncwang <juncwang@live.com>
2020-05-28 18:15:37 +0800

    webpack update

--- npm/webpack/README.md
+++ npm/webpack/README.md
@@ -1,13 +1,32 @@
 ### 使用 webpack
 
 * 需要安装包
-    * "css-loader"  解析 css
+    
+    * "webpack" 打包 `可定义到 package.json 中使用 npm run 启动`
+    * "webpack-cli" 打包工具
+    * "webpack-dev-server" 开发者模式 `可定义到 package.json 中使用 npm run 启动`
+
     * "file-loader" 解析 file
     * "html-loader" 解析 html
     * "html-webpack-plugin" html 导出插件
+    * "url-loader" 解析其他文件
+
     * "less-loader" 解析 less
+    * "css-loader"  解析 css
     * "style-loader" 解析 css less 基础
-    * "url-loader" 解析图片
-    * "webpack" 基础
-    * "webpack-cli" 打包工具
-    * "webpack-dev-server" 开发者模式
+    * "mini-css-extract-plugin" 单独处理 css 文件插件
+    * "postcss-loader": 处理 css 兼容性问题
+    * "postcss-preset-env" 处理 css 兼容性问题
+    * "optimize-css-assets-webpack-plugin" 压缩 css 插件
+
+    * "eslint" 语法检查
+    * "eslint-loader" 解析语法检查
+    * "eslint-config-airbnb-base" 代码规范 不包含react
+    * "eslint-config-airbnb" airbnb 代码规范 包含react
+    * "eslint-plugin-import" 使用时需要用到的插件
+
+    * "@babel/core" 处理 js 兼容性问题
+    * "babel-loader 处理 js 兼容性问题
+    * "@babel/preset-env" 处理基本兼容性问题
+    * "@babel/polyfill" 处理全部兼容性问题
+    * "core-js" 按需做 js 兼容性处理
--- npm/webpack/webpack.config.js
+++ npm/webpack/webpack.config.js
@@ -1,5 +1,13 @@
 const { resolve } = require('path')
+// html 独立文件插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// css 独立文件插件
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// css 压缩文件插件
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+// 设置 node 的环境变量
+// process.env.NODE_ENV = "developement"
 
 module.exports = {
     // 入口文件
@@ -15,6 +23,70 @@ module.exports = {
     module: {
         rules: [
             {
+                // 处理 js 兼容性 babel-loader @babel/preset-env @babel/core
+                // 1 基本 js 兼容性处理 -- @babel/preset-env  == 无法对 promise 等新东西进行处理
+                // 2 全部 js 兼容性处理 -- @babel/polyfill == 体积太大
+                //        * 只需要在相应的 js 代码文件内 import 即可
+                // 3 按需兼容性处理: 按需加载 --> core-js
+                test: /\.js$/,
+                // 不处理第三方包内的代码
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                // 前两中 1 + 2 适用
+                // options: {
+                //     // 预设 指示 babel 做什么兼容处理
+                //     presets: ['@babel/preset-env']
+                // }
+                // 第三种按需处理兼容性问题 1 + 3
+                options: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                // 按需加载
+                                useBuiltIns: 'usage',
+                                // 指定 core-js 版本
+                                corejs: {
+                                    version: 3
+                                },
+                                // 指定具体的兼容性做到哪个浏览器版本
+                                targets: {
+                                    chrome: '60',
+                                    firefox: '60',
+                                    ie: '9',
+                                    safari: '10',
+                                    edge: '17'
+                                }
+                            }
+                        ]
+                    ]
+                }
+            },
+            {
+                // 语法检查 eslint-loader eslint
+                // 设置检查规则
+                // eslint-config-airbnb 包含 react
+                // eslint-config-airbnb-base 不包含 react
+                // 以上需要 eslint-plugin-import 库
+                /**
+                 * // package.json 添加一个对象
+                 * "eslintConfig": {
+                 *      "extends": "airbnb-base"
+                 *      "extends": "airbnb" 如果是需要包含 react 就用这个
+                 * }
+                 *
+                 * // 在代码中加入 // eslint-disable-next-line 表示下一行不进行规则检查
+                 */
+                test: /\.js$/,
+                // 排除第三方库的代码监测
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    // 按规则自动修复 eslint 错误
+                    fix: true
+                }
+            },
+            {
                 // 处理 less 资源
                 // 文件类型
                 test: /\.less$/,
@@ -29,8 +101,47 @@ module.exports = {
                 // 处理 css 资源
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    // 该 loader 把 css 放入 js 中 (css 与下 loader 二选一)
+                    // 'style-loader',
+                    // 提取 js 中的 css 成单独文件 (css 与上 loader 二选一)
+                    MiniCssExtractPlugin.loader,
+                    // 解析 css 文件
+                    'css-loader',
+                    // 使用 postcss-loader 加载 提高样式兼容性
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                // 使用 postcss 插件
+                                // 帮助 browserslist 里面的配置, 通过配置加载指定兼容性样式
+                                /**
+                                 * // package.json 添加一个对象
+                                 * "browserslist"": {
+                                 *     // 开发环境配置
+                                 *     // 需要设置 node 环境变量: process.env.NODE_ENV = developement 才会使用开发模式
+                                 *     "development": [
+                                 *          // 兼容最新的 浏览器 版本
+                                 *          "last 1 chrome version",
+                                 *          "last 1 firefox version",
+                                 *          "last 1 safari version"
+                                 *     ],
+                                 *     // 生成环境配置
+                                 *     // 默认使用生成配置
+                                 *     "production": [
+                                 *          // 大于 99.8% 的浏览器
+                                 *          ">0.01%",
+                                 *          // 不需要 ie10 之内已经没人用的浏览器兼容
+                                 *          "not dead",
+                                 *          // 不需要 op_mini 所有版本的浏览器兼容
+                                 *          "not op_mini all"
+                                 *     ]
+                                 * }
+                                 */
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
                 ]
             },
             {
@@ -67,8 +178,23 @@ module.exports = {
         // 配置 html 输出
         new HtmlWebpackPlugin({
             // 模板文件路径
-            template: "./src/index.html"
-        })
+            template: "./src/index.html",
+            // 压缩 html 代码
+            minify: {
+                // 移出折叠结构
+                collapseWhitespace: true,
+                // 移出注释
+                removeComments: true,
+            }
+        }),
+        // 配置 css 输出
+        new MiniCssExtractPlugin({
+            // 文件输出路径
+            filename: 'css/built.css'
+        }), // 使用 css 配置时需要把 css loader 内容进行更改
+        // 配置 css 压缩
+        new OptimizeCssAssetsWebpackPlugin(),
+
     ],
     // 模式
     mode: "development", // 'development' or 'production'
